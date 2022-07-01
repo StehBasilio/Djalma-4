c32a1f42016c4960e791e4c83657e09f19ede099
basil <basil@DESKTOP-BMHL20J>
2022-07-01 14:32:12 -0300

    Adicionar arquivos de projeto.

---
+++ Djalma 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djalma_4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int s;
+			for (s = 1; s <= 50;)
+			{
+				Console.WriteLine(s);
+				s++;
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
